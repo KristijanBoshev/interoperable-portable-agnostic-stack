102fe7464619913a1e3286bc0ea0ddf2a4c757a2
KristijanBoshev <kristijan.boshev.1@students.finki.ukim.mk>
2026-02-04 13:49:53 +0100

    chore: add s3 lockfile

--- infra/versions.tf
+++ infra/versions.tf
@@ -1,12 +1,14 @@
 terraform {
   required_version = ">= 1.5.0"
 
-  backend "s3" {}
+  backend "s3" {
+    use_lockfile = true
+  }
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.28"
+      version = "~> 6.30"
     }
   }
 }
